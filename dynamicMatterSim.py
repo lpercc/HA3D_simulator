d216170968ec397aada4b84f8385e119d3100a24
lpercc <1305269712@qq.com>
2024-02-27 20:22:36 +0800

    updata get dynamic

--- dynamicMatterSim.py
+++ dynamicMatterSim.py
@@ -96,7 +96,7 @@ class HC_Simulator(MatterSim.Simulator):
             #print('POST response: ', response.text)
         else:
             #super().makeAction(index, heading, elevation)
-            self.state_index = index
+            self.state_index = index[0]
     
     def getState(self, num_frames):
         if self.remote:
@@ -108,9 +108,9 @@ class HC_Simulator(MatterSim.Simulator):
             state.video = self.HCFusion(state, num_frames=num_frames)
         else:
             print(self.scanId,self.viewpointId,self.state_index)
-            matching_dicts = [dic for dic in state_list if dic['scanId'] == scanId]
-            matching_dicts = [dic for dic in matching_dicts if dic['viewIndex'] == state_index]
-            matching_dicts = [dic for dic in matching_dicts if dic['location']['viewpointId'] == viewpointId]
+            matching_dicts = [dic for dic in self.state_list if dic['scanId'] == self.scanId]
+            matching_dicts = [dic for dic in matching_dicts if dic['viewIndex'] == self.state_index]
+            matching_dicts = [dic for dic in matching_dicts if dic['location']['viewpointId'] == self.viewpointId]
             first_matching_dict = next(dic for dic in self.state_list if dic['scanId'] == self.scanId and dic['viewIndex'] == self.state_index and dic['location']['viewpointId'] == self.viewpointId)
             o_state = first_matching_dict
             state = HC_SimState(o_state)
