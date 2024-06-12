0d7ffd73c5a8e8d0209e1650ef3a3fe6afccea7f
lpercc <lperlpm@gmail.com>
2024-06-13 00:20:18 +0800

    update branch

--- src/utils/concat_skybox.py
+++
@@ -1,6 +0,0 @@
-import imageio
-
-def concat(image_path, width):
-    downsize_image = imageio.imread(image_path)[:,width:-width,:]
-    return downsize_image
-    
--- ui/README.md
+++
@@ -1,5 +0,0 @@
-# HC-VLN_data_annotation
-
-```bash
-pyuic5 -x input.ui -o output.py
-```
